47116a6d4151ccd4c87ad32845f0bad1d3f6c776
Soleyman Shahir <Soleymanshahir@yahoo.com>
2020-04-25 19:48:37 +0100

    Updates

---
+++ precache-manifest.a810056e5e9a0488ff1564f1621a4413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38f10facff000f68da2ead40aeef9b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "75438338003b5d765694",
+    "url": "/monsters-rolodex/static/css/main.e466fb56.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75438338003b5d765694",
+    "url": "/monsters-rolodex/static/js/main.9c4e9756.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
